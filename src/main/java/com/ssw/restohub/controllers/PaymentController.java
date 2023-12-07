32f090b21af46d015f89ce061d4e2a7fcb8b8a6d
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2023-12-06 20:52:59 -0500

    cross origin resolve

--- src/main/java/com/ssw/restohub/controllers/PaymentController.java
+++ src/main/java/com/ssw/restohub/controllers/PaymentController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class PaymentController {
 
     private StripeService paymentsService;
